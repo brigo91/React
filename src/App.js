5f8e13b4fadb95a36b2a853cad473f0fae30596f
brigo <brigovaczbalazs@hotmail.com>
2021-05-08 13:40:52 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
 	render() {
-		/* return (
+		return (
 			<div className="App">
 				<h3>Hi, I'm a React App!</h3>
 			</div>
-		); */
-		return React.createElement('div', {className: 'App'}, React.createElement('h3', null, 'Does this work now?'));
+		);
+		//return React.createElement('div', {className: 'App'}, React.createElement('h3', null, 'Does this work now?'));
 	}
 }
 
