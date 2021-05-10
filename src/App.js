26a66b2fa4b1451f9b0a5c3242bc0e63621bf740
brigo <brigovaczbalazs@hotmail.com>
2021-05-10 22:35:00 +0200

    Manipulate state

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
 			{name: "Max", age: 28},
 			{name: "Manu", age: 29},
 			{name: "Stephanie", age: 26},
-		]
+		],
+		otherState: 'some other value'
 	}
 
 	switchNameHandler = () => {
 		console.log('Was clicked');
+		//DO NOT USE THIS: this.state.persons[0].name = "Maximilian";
+		this.setState({
+			persons: [
+				{name: "Maximilian", age: 28},
+				{name: "Manu", age: 29},
+				{name: "Stephanie", age: 27},
+			]
+		});
+		console.log(this.state);
 	}
 
 	render() {
