fce9a46907c3b7a493353779b5acee15590cf265
brigo <brigovaczbalazs@hotmail.com>
2021-06-24 20:35:29 +0200

    Component creation lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		console.log('[App.js] constructor');
+	}
+
 	state = {
 		persons: [
 			{id: 'asafa1', name: "Max", age: 28},
@@ -13,6 +18,19 @@ class App extends Component {
 		],
 		otherState: 'some other value',
 		showPersons: false
+	}
+
+	static getDerivedStateFromProps(props, state){
+		console.log('[App.js] getDerivedSateFromProps', props);
+		return state;
+	}
+
+	/* componentWillMount(){
+		console.log('[App.js] componentWillMount');
+	} */
+
+	componentDidMount(){
+		console.log('[App.js] componentDidMount');
 	}
 
 	deletePersonHandler = (personIndex) => {
@@ -48,8 +66,8 @@ class App extends Component {
 	}
 
 	render() {
+		console.log('[App.js] render' );
 		let persons = null;
-		
 
 		if (this.state.showPersons){
 			persons = <Persons 
