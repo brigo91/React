feb6cad3bb1b36db14454538d4790def85fabb7c
brigo <brigovaczbalazs@hotmail.com>
2021-06-30 19:39:49 +0200

    Use useEffect() in functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(()=>{
+        console.log('[App.js] useEffect');
+        //Http request...
+    });
+
     const assingendClasses = [];
     let btnClass = '';
     if (props.showPersons){
