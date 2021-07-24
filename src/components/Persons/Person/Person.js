13cb2c70f22b29bd57ecbbfa9762ffb6eedc42ca
brigo <brigovaczbalazs@hotmail.com>
2021-07-24 11:15:50 +0200

    contextType and useContext()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,20 +11,24 @@ class Person extends Component {
         super(props);
         this.inputElementRef = React.createRef();
     }
+
+    static contextType = AuthContext;
+
     componentDidMount(){
         //this.inputElement.focus();
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
     }
 
     render(){
         console.log('[Person.js] rendering...');
         return (
         <Aux>
-            <AuthContext.Consumer>
-                {context => 
-                    context.authenticated ? <p>Authenticated!</p> : <p>Please log in!</p>
+                {
+                    this.context.authenticated ?
+                        <p>Authenticated!</p> :
+                        <p>Please log in!</p>
                 }
-            </AuthContext.Consumer>
             <p onClick={this.props.click}>
                 I'm {this.props.name} and I'm {this.props.age} years old!
             </p>
