40d221a8e363e355dae35fbcaf52a7f1224f1153
brigo <brigovaczbalazs@hotmail.com>
2021-08-25 14:11:10 +0200

    Navigating to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component{
 	}
 
 	componentDidMount(){
+		console.log(this.props);
 		axios.get('https://react-my-burger-48074-default-rtdb.europe-west1.firebasedatabase.app/igredients.json')
 			.then(response => {
 				this.setState({ingredients: response.data});
@@ -93,28 +94,29 @@ class BurgerBuilder extends Component{
 
 	purchaseContinueHandler = () => {
 		//alert('You continue!');
-		this.setState({loading: true});
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Brigovácz Balázs',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41351',
-					country: 'Germany'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
-		}
-		axios.post('orders.json', order)
-			.then(response => {
-				this.setState({loading: false, purchasing: false});
-			})
-			.catch(error => {
-				this.setState({loading: false, purchasing: false});
-			});
+		// this.setState({loading: true});
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Brigovácz Balázs',
+		// 		address: {
+		// 			street: 'Teststreet 1',
+		// 			zipCode: '41351',
+		// 			country: 'Germany'
+		// 		},
+		// 		email: 'test@test.com'
+		// 	},
+		// 	deliveryMethod: 'fastest'
+		// }
+		// axios.post('orders.json', order)
+		// 	.then(response => {
+		// 		this.setState({loading: false, purchasing: false});
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({loading: false, purchasing: false});
+		// 	});
+		this.props.history.push('/checkout');
 	}
 
 	render(){
