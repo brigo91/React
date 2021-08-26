881ff8379af437caed103fcfedd99f7990cbf331
brigo <brigovaczbalazs@hotmail.com>
2021-08-26 17:52:38 +0200

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+
+import Counter from './containers/Counter/Counter';
 import './App.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h3>Hi, I'm a React App!</h3>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+       <Counter />
+      </div>
+    );
+  }
 }
 
 export default App;
