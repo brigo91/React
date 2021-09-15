9238112d3ff5ff596130408e649bec06609349c9
brigo <brigovaczbalazs@hotmail.com>
2021-09-15 12:19:51 +0200

    Adjusting checkout and contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Button from '../../../components/UI/Button/Button';
@@ -101,7 +102,7 @@ class ContactData extends Component{
 		}
 		this.setState({loading: true});
 		const order = {
-			ingredients: this.props.ingredients,
+			ingredients: this.props.ings,
 			price: this.props.price,
 			orderData: formData
 		}
@@ -195,4 +196,11 @@ class ContactData extends Component{
 	}
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+	return {
+		ings: state.ingredients,
+		price: state.totalPrice
+	};
+};
+
+export default connect(mapStateToProps)(ContactData);
