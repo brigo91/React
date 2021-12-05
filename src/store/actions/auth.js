02a476ae32ae5fd1cddedc921f9a6adb90324fa0
brigo <brigovaczbalazs@hotmail.com>
2021-12-05 16:49:24 +0100

    Fxing connect + routing errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -33,7 +33,7 @@ export const logout = () => {
 
 export const checkAuthTimeout = (expirationTime) => {
 	return dispatch => {
-		setTimeout(() =>{
+		setTimeout(() => {
 			dispatch(logout());
 		}, expirationTime * 1000 );
 	}
@@ -56,7 +56,7 @@ export const auth = (email, password, issignUp) => {
 				console.log(response);
 				const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
 				localStorage.setItem('token', response.data.idToken);
-				localStorage.setItem('experationDate', expirationDate);
+				localStorage.setItem('expirationDate', expirationDate);
 				localStorage.setItem('userId', response.data.localId);
 				dispatch(authSuccess(response.data.idToken, response.data.localId));
 				dispatch(checkAuthTimeout(response.data.expiresIn));
@@ -81,10 +81,12 @@ export const authCheckState = () => {
 			dispatch(logout());
 		}else{
 			const expirationDate = new Date(localStorage.getItem('expirationDate'));
-			if ( expirationDate > new Date() ) {
+			if ( expirationDate <= new Date() ) {
+				dispatch(logout());
+			}else{
 				const userId = localStorage.getItem('userId');
 				dispatch(authSuccess(token, userId));
-				dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+				dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
 			} 
 		}
 	}
