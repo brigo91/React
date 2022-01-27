8c6c8674de0353f990b06b2585500d7ea6f4a133
brigo <brigovaczbalazs@hotmail.com>
2022-01-27 20:25:31 +0100

    Setting up a project

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const authIndexPage = (props) => {
+	return (
+		<div>
+			<h1>The Auth Index Page</h1>
+		</div>
+	);
+};
+
+export default authIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const indexPage = (props) => {
+	return (
+		<div>
+			<h1>The main Page</h1>
+		</div>
+	);
+};
+
+export default indexPage;
