3d15bdd3bcdc19648cac03a9763e11b67edfc96f
brigo <brigovaczbalazs@hotmail.com>
2022-01-27 22:06:44 +0100

    Handling 404 errors

---
+++ pages/_error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const errorPage = (props) => {
+	return (
+		<div>
+			<h1>Ooops something went wrong</h1>
+			<p>Go to <Link href="/"><a>Going back</a></Link></p>
+		</div>
+	);
+};
+
+export default errorPage;
