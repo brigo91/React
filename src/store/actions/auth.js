c1ae334f105f4db3c674743a9129d3ff69e9b4aa
brigo <brigovaczbalazs@hotmail.com>
2022-02-01 23:43:38 +0100

    Handling authentication with a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from 'axios';
 
 export const authStart = () => {
 	return {
@@ -44,30 +43,12 @@ export const checkAuthTimeout = (expirationTime) => {
 	}
 };
 
-export const auth = (email, password, issignUp) => {
-	return dispatch => {
-		dispatch(authStart());
-		const authData = {
-			email: email,
-			password: password,
-			returnSecureToken: true
-		};
-		let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCfVHOeqG_zXYIcnweqrHhi0CeCxB0SHOw';
-		if(!issignUp){
-			url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCfVHOeqG_zXYIcnweqrHhi0CeCxB0SHOw';
-		}
-		axios.post(url, authData)
-			.then( response => {
-				const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-				localStorage.setItem('token', response.data.idToken);
-				localStorage.setItem('expirationDate', expirationDate);
-				localStorage.setItem('userId', response.data.localId);
-				dispatch(authSuccess(response.data.idToken, response.data.localId));
-				dispatch(checkAuthTimeout(response.data.expiresIn));
-			})
-			.catch(err => {
-				dispatch(authFail(err.response.data.error));
-			});
+export const auth = (email, password, isSignup) => {
+	return {
+		type: actionTypes.AUTH_USER,
+		email: email,
+		password: password,
+		isSignup: isSignup
 	};
 };
 
