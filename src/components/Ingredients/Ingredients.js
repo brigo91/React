f1d16cb1f30d198b8dca7b8a34176e9696e2f951
brigo <brigovaczbalazs@hotmail.com>
2022-02-14 20:03:07 +0100

    Sending http request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,18 @@ const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      {id: Math.random().toString(), ...ingredient}
-    ]);
+    fetch('https://react-hooks-update-1838d-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',{
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        {id: Math.random().toString(), ...ingredient}
+      ]);
+    });
   };
 
   return (
